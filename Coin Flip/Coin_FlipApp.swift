b56002f591b79140ab7923edf41429d5e3db9e49
Benny Chopra <bchopra@bsd220.org>
2024-11-22 09:35:31 -0600

    Initial Commit

---
+++ Coin Flip/Coin_FlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_FlipApp.swift
+//  Coin Flip
+//
+//  Created by Benny Chopra on 11/22/24.
+//
+
+import SwiftUI
+
+@main
+struct Coin_FlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
